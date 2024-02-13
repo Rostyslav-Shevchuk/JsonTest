bfabd2307c7c5faf5345676135e215b4818ea006
Rostyslav <159663154+Rostyslav-Shevchuk@users.noreply.github.com>
2024-02-13 22:27:12 +0200

    Initial Commit

---
+++ JsonTest/JsonTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonTestApp.swift
+//  JsonTest
+//
+//  Created by Ростислав on 13.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct JsonTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
